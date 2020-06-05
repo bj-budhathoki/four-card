1c9f608d38cde1c42e7925764b4a3554a4dbbfd4
bj-budhathoki <bijayabudhathoki98@gmail.com>
2020-06-05 13:38:47 +0545

    inital commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Card from "./components/Card.svelte";
+</script>
+
+<style>
+
+</style>
+
+<main>
+  <header>
+    <h1>Reliable, efficinet delivery</h1>
+    <h1>Powere by Technology</h1>
+    <p>
+      Our Artificial intelligence powered tools use millions of project data
+      points to ensure that your project issuccessful.
+    </p>
+  </header>
+  <Card
+    title="Supervisor"
+    text="Monitors activity to identify project roadblocks"
+    icon="icon-supervisor.svg" />
+</main>
---
+++ src/components/Card.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let title;
+  export let text;
+  export let icon;
+</script>
+
+<div class="card">
+  <h3>{title}</h3>
+  <p>{text}</p>
+  <img src={`assets/images/${icon}`} alt={title} />
+</div>
